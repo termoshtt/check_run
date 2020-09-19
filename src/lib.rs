5cdfd7b6279aa675da2068c31a8e414fb10203b6
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-09-19 19:48:04 +0900

    Use thiserror instead of failure

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,31 @@
-#[macro_use]
-extern crate failure;
-
 use std::process::Command;
 
-#[derive(Debug, Fail)]
-pub enum ExternalCommandError {
-    #[fail(display = "Command not found: {}", command)]
+#[derive(Debug, thiserror::Error)]
+pub enum CommandError {
+    #[error("Command not found: {command}")]
     CommandNotFound { command: String },
-    #[fail(display = "Non-zero exit-code = {}: {}", exit_code, command)]
+
+    #[error("Non-zero exit-code = {exit_code}: {command}")]
     NonZeroExitCode { command: String, exit_code: i32 },
 }
 
 /// Run `std::process::Command` and check error into `Fail` trait
 pub trait CheckRun {
     /// Run `std::process::Command` and check error
-    fn check_run(&mut self) -> Result<(), ExternalCommandError>;
+    fn check_run(&mut self) -> Result<(), CommandError>;
 }
 
 impl CheckRun for Command {
-    fn check_run(&mut self) -> Result<(), ExternalCommandError> {
+    fn check_run(&mut self) -> Result<(), CommandError> {
         let st = self.status().map_err(|_error| {
             let command = format!("{:?}", self);
-            ExternalCommandError::CommandNotFound { command }
+            CommandError::CommandNotFound { command }
         })?;
         match st.code() {
             Some(exit_code) => {
                 if exit_code != 0 {
                     let command = format!("{:?}", self);
-                    Err(ExternalCommandError::NonZeroExitCode { command, exit_code })
+                    Err(CommandError::NonZeroExitCode { command, exit_code })
                 } else {
                     Ok(())
                 }
